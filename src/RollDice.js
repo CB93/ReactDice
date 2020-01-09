0484f4e0cc9a15e335687b02926349ca595ab268
CB93 <caleb_1993_@hotmail.com>
2020-01-09 01:29:20 -0800

    feat/dice roll finished

--- src/RollDice.js
+++ src/RollDice.js
@@ -1,20 +1,36 @@
 import React, { Component } from "react";
 import Dice from './Dice'
-
+import CONSTANTS from './Constants'
 class RollDice extends Component {
+
+
   constructor() {
     super()
     this.state = {
-      diceOne : "one",
-      diceTwo : "one"
+      diceOne: "one",
+      diceTwo: "one"
     }
+    this.roll = this.roll.bind(this)
   }
-  
+
+
+  // Rolls a value between 1 and 6 for both die and converts value into a word.
+  roll() {
+    const value1 = CONSTANTS.ROLLNUMS[Math.floor(Math.random() * 6)]
+    const value2 = CONSTANTS.ROLLNUMS[Math.floor(Math.random() * 6)]
+
+    this.setState({
+      diceOne: value1,
+      diceTwo: value2,
+    })
+  }
+
   render() {
     return (
       <div>
         <Dice value={this.state.diceOne} />
-        <Dice value={this.state.diceTwo}/>
+        <Dice value={this.state.diceTwo} />
+        <button onClick={this.roll}>Click to roll</button>
       </div>
     )
   }
